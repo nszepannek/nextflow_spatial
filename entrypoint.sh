d82ff5db43bf5c7a25148ec92b88faa6d3260d87
Nathalie Szepannek <nathalie.szepannek@fh-campuswien.ac.at>
2025-07-02 23:37:00 +0200

    $@

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
 
-srun --container-image=nvcr.io/muwsc/ifi/nf-spatial:latest --container-workdir=/root/nextflow_spatial nextflow run nf_spatial.nf \
+srun --container-image=nvcr.io/muwsc/ifi/nf-spatial:latest --container-workdir=/root/nextflow_spatial nextflow run nf_spatial.nf  \
   -with-report reports/report_${TIMESTAMP}.html \
   -with-trace reports/trace_${TIMESTAMP}.txt \
   -with-dag reports/flowchart_${TIMESTAMP}.png \
-  -with-timeline reports/timeline_${TIMESTAMP}.html
+  -with-timeline reports/timeline_${TIMESTAMP}.html $@
